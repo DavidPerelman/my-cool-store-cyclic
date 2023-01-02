caacf4f76aead47f7316f643ab22a0d8138bc718
David Perelman <dperelman3@gmail.com>
2023-01-02 21:44:30 +0200

    setup CartContext

--- clinet/src/components/Cart/Cart/Cart.js
+++ clinet/src/components/Cart/Cart/Cart.js
@@ -3,6 +3,16 @@ import Modal from '../../UI/Modal/Modal';
 import classes from './Cart.module.css';
 
 const Cart = ({ onCloseCart }) => {
+  const product = {
+    id: 9,
+    title: 'WD 2TB Elements Portable External Hard Drive - USB 3.0 ',
+    price: 64,
+    description:
+      'USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system',
+    category: 'electronics',
+    image: 'https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg',
+  };
+
   const cartItems = (
     <ul className={classes['cart-items']}>
       {[
@@ -15,8 +25,17 @@ const Cart = ({ onCloseCart }) => {
           amount: 2,
           price: 12.99,
         },
+        {
+          id: 9,
+          name: 'WD 2TB Elements Portable External Hard Drive - USB 3.0 ',
+          price: 64,
+          description:
+            'USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system',
+          category: 'electronics',
+          image: 'https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg',
+        },
       ].map((item) => {
-        <li>{item.name}</li>;
+        return <li>{item.name}</li>;
       })}
     </ul>
   );
