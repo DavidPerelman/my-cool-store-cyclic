9e445b9113724869739c6b3fa239ef1c0e390363
David Perelman <dperelman3@gmail.com>
2023-01-02 16:37:57 +0200

    setup Login: emailChangeHandler

--- clinet/src/components/Users/Login/Login.js
+++ clinet/src/components/Users/Login/Login.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../../UI/Input/Input';
 import classes from './Login.module.css';
 
 const Login = ({ goToRegister }) => {
+  const [enteredEmail, setEnteredEmail] = useState('');
+
+  const emailChangeHandler = (e) => {
+    setEnteredEmail(e.target.value);
+  };
+
   return (
     <div className={classes.login}>
       <h1>Login</h1>
       <form className={classes['login-form']}>
-        <Input id='email' label='E-Mail' type='email' />
+        <Input
+          id='email'
+          label='E-Mail'
+          type='email'
+          onChange={emailChangeHandler}
+        />
         <Input id='password' label='Password' type='password' />
       </form>
       <button onClick={goToRegister}>Register</button>
