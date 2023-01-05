2b6916b58a4434994d461847ef0c5f86b945f7fc
David Perelman <dperelman3@gmail.com>
2023-01-05 15:18:27 +0200

    setup Router and Home

---
+++ clinet/src/Router.js
@@ -0,0 +1,1 @@
+import { Route, Routes, useLocation } from 'react-router-dom';
---
+++ clinet/src/components/Products/ProductDetails/ProductDetails.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './ProductDetails.module.css';
+
+const ProductDetails = () => {
+  return <h1>Product Details Page</h1>;
+};
+
+export default ProductDetails;
