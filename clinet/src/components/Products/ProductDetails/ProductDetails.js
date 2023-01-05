a12931cdc36e2f4ddc6b70195e707b680a8e3ef7
David Perelman <dperelman3@gmail.com>
2023-01-05 20:16:29 +0200

    setup fetch data in ProductDetails

--- clinet/src/components/Products/ProductDetails/ProductDetails.js
+++ clinet/src/components/Products/ProductDetails/ProductDetails.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import classes from './ProductDetails.module.css';
 
 const ProductDetails = () => {
+  const [product, setProduct] = useState({});
   const { productId } = useParams();
+
+  useEffect(() => {
+    fetch(`api/products/${productId}`)
+      .then((res) => res.json())
+      .then((data) => setProduct(data.products));
+  }, []);
+
   console.log(productId);
   return <h1>Product {productId} Details Page</h1>;
 };
