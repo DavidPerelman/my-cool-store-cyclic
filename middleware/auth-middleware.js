56578bbbbb98ef1528fa7993bef487338d51e5de
David Perelman <dperelman3@gmail.com>
2023-01-09 15:27:17 +0200

    setup authMiddlware

---
+++ middleware/auth-middleware.js
@@ -0,0 +1,17 @@
+const { auth } = require('firebase-admin');
+
+const authMiddlware = async (req, res, next) => {
+  const token = req.headers.authorization.split(' ')[1];
+  try {
+    const decodeValue = await auth().verifyIdToken(token);
+    if (decodeValue) {
+      console.log(decodeValue);
+      return next();
+    }
+    return res.json({ message: 'Unauthorized' });
+  } catch (e) {
+    return res.json({ message: 'Internal Error' });
+  }
+};
+
+module.exports = { authMiddlware };
